bd77444f0e38bfde0cfbb599e03fa5ca8b9babda
antoineb19 <antoinebenoit@msn.com>
2016-06-14 20:52:33 +0200

    Who has two thumbs and remembers the rudiments of his linear algebra courses?  Apparently, this guy.

--- publish.sh
+++ publish.sh
@@ -1,11 +1,2 @@
 git add --all
-comment=`curl -s 'http://whatthecommit.com/' | grep '<p>' | cut -c4-`
-guillemet="'"
-c=$guillemet$comment$guillemet
-
-git commit
-i
-c
-^C
-:wq
-git push
+git commit -m "$(curl -s http://whatthecommit.com/index.txt)";
