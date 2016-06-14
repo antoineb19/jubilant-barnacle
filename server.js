5c8c144f0d4350ae69f3ca721613715e30355fd9
antoineb19 <antoinebenoit@msn.com>
2016-06-14 20:03:02 +0200

    server

---
+++ server.js
@@ -0,0 +1,21 @@
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT=80;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.writeHead(200);
+    response.end('Salut tout le monde !');
+    console.log('Requête reçue !');
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
