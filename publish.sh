0e634c1519a526491751f9535684e41c96aa4955
antoineb19 <antoinebenoit@msn.com>
2016-06-14 20:16:41 +0200

    $comment

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 git add --all
-comment=curl -s 'http://whatthecommit.com/' | grep '<p>' | cut -c4-
-git commit -m 'comment'
+comment=`curl -s 'http://whatthecommit.com/' | grep '<p>' | cut -c4-`
+echo $comment
+git commit -m '$comment'
 git push
