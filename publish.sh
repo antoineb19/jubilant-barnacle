ad3135c4b87fd6972b4e97135b36cb11455a7436
antoineb19 <antoinebenoit@msn.com>
2016-06-14 20:43:29 +0200

    'oops!'

--- publish.sh
+++ publish.sh
@@ -2,12 +2,6 @@ git add --all
 comment=`curl -s 'http://whatthecommit.com/' | grep '<p>' | cut -c4-`
 guillemet="'"
 c=$guillemet$comment$guillemet
-total=""
-for i in $(seq 1 ${#c}); do
-	char=$(echo $c | cut -c$i)
-	echo $char
-	total=$total$(echo $c | cut -c$i)
-done
-echo $total
-#git commit -m $c
-#git push
+
+echo $(git commit -m $c)
+git push
